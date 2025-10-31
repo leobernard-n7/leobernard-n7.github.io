c90767ee6d0b819c00b7a2ca570ea11873b59297
Léo Bernard <54135715+Yolwoocle@users.noreply.github.com>
2025-10-31 22:36:34 +0100

    zerazeraz

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,11 @@
 	let { children }: {
 		children: Snippet<[]>;
 	} = $props();
+
+	// If you're using a fallback (i.e. SPA mode) you don't need to prerender all
+	// pages by setting this here, but should prerender as many as possible to
+	// avoid large performance and SEO impacts
+	export const prerender = true;
 </script>
 
 <svelte:head>
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
